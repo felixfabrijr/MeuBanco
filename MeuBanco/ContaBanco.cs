0887a2f242c8af4904fb32e7f659d728856a6ddd
felixfabri <felix.fabrijr@gmail.com>
2023-03-13 12:37:38 +0000

    Add project files.

---
+++ MeuBanco/ContaBanco.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuBanco
+{
+    public class ContaBanco
+    {
+        public string Numero { get; }
+        public string Dono { get; set; }
+        public decimal Saldo
+        {
+            get
+            {
+                decimal saldo = 0;
+                foreach (var item in todasTransacoes)
+                {
+                    saldo += item.Valor;
+                }
+                return saldo;
+            }
+        }
+
+        public static int numeroConta = 1234567890;
+
+        private List<Transacao> todasTransacoes = new List<Transacao>();
+
+
+        public ContaBanco(string nome, decimal valor)
+        {
+            this.Dono= nome;
+            this.Saldo= valor;
+
+            numeroConta++;
+
+            this.Numero= numeroConta.ToString();
+        }
+        public void Depositar()
+        {
+        }
+        public void Sacar()
+            {
+
+            
+        }
+    }
+}
---
+++ MeuBanco/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MeuBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuBanco.ContaBanco contaB = new ContaBanco("Diogo", 10000);
+            Console.WriteLine($"conta {contaB.Numero} de {contaB.Dono} foi criada com {contaB.Saldo}");
+        }
+    }
+}
